5c42c7fa003ec5636296f958850d4e1ee3e634a7
atk1432 <hiepnguyenvietatk@gmail.com>
2022-02-17 16:44:38 +0700

    add animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,31 @@
 function main() {
     const choiceHRight = document.querySelectorAll('.header__category-right span')    
     const selectBox = document.querySelectorAll('.select-box')
+    const mainMenuIcon = document.querySelector('.main-menu-wrap__icon i')
+    const mainMenu = document.querySelector('.main-menu-wrap .col')
 
     choiceHRight.forEach((element) => {
         element.onclick = function(e) {
             e.stopPropagation()
-            var selectBox = this.nextElementSibling
-            selectBox.style.display = 'block'
+            selectBox.forEach((select) => {
+                select.style.display = 'none'
+            })
+            this.nextElementSibling.style.display = 'block'
             // selectBox.style.display = selectBox.style.display ? 'none' : 'block' 
         }
     })
+
+    mainMenuIcon.onclick = function() {
+        mainMenu.classList.toggle('active-main-menu')
+    }
+
 
     document.body.onclick = function() {
         selectBox.forEach((e) => {
             e.style.display = 'none'
         })
     }
+
 }
 
 main()
