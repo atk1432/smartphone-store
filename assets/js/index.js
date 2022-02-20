6906ff1cef91dadeb04f16bf05988440f6e76c17
atk1432 <hiepnguyenvietatk@gmail.com>
2022-02-20 22:10:22 +0700

    slider slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,60 @@ function main() {
     const selectBox = document.querySelectorAll('.select-box')
     const mainMenuIcon = document.querySelector('.main-menu-wrap__icon i')
     const mainMenu = document.querySelector('.main-menu-wrap .col')
+      
     
+    choiceHRight.forEach((element) => {
+        element.onclick = function(e) {
+            e.stopPropagation()
+            selectBox.forEach((select) => {
+                select.style.display = 'none'
+            })
+            this.nextElementSibling.style.display = 'block'
+            // selectBox.style.display = selectBox.style.display ? 'none' : 'block' 
+        }
+    })
+    
+    
+    mainMenuIcon.onclick = function() {
+        mainMenu.classList.toggle('active-main-menu')
+    }
+    
+    document.body.onclick = function() {
+        selectBox.forEach((e) => {
+            e.style.display = 'none'
+        })
+    }
+    
+    function processBxPaper() {
+        const bxPapers = document.querySelectorAll('.slider-wrap__bx-paper-item')
+        const sliderWrap = document.querySelector('.slider-wrap')
+        const angleLeft = document.querySelector('.slider-wrap__angle-left')
+        const angleRight = document.querySelector('.slider-wrap__angle-right')
+
+        sliderWrap.onmouseover = function() {
+            angleLeft.style.transform = 'translate(32px)'
+            angleRight.style.transform = 'translate(-32px)'
+        }
+
+        sliderWrap.onmouseout = function() {
+            angleLeft.style.transform = angleRight.style.transform = ''
+        }
+
+        bxPapers.forEach(function(e) {
+
+            e.onclick = function() {
+
+                bxPapers.forEach((e) => {
+                    e.classList.remove('slider-wrap__bx-paper-item--active')
+                })
+
+                this.classList.toggle('slider-wrap__bx-paper-item--active')
+                // console.log(this)
+            }
+        })
+    }
+
+
     function processScroll() {
         const mainMenu = document.querySelector('.main-menu')
         const slider = document.querySelector('.slider')
@@ -11,7 +64,6 @@ function main() {
         const header = document.querySelector('.header')
 
         const height = siteBranding.offsetHeight + header.offsetHeight
-        console.log(height)
 
         document.onscroll = function() {
             if (document.documentElement.scrollTop >= height) {
@@ -26,49 +78,31 @@ function main() {
 
 
     function processSliderLink() {
-        const sliderLink = document.querySelector('.slider__item-info-now-text')
-        const sliderSpan = document.querySelector('.slider__item-info-now-text span')      
-        const sliderIcon = document.querySelector('.slider__item-info-now-icon')
+        const sliderLink = document.querySelectorAll('.slider__item-info-now-text')
+        const sliderSpan = document.querySelectorAll('.slider__item-info-now-text span')      
+        const sliderIcon = document.querySelectorAll('.slider__item-info-now-icon')
 
+        sliderLink.forEach(function(e) {
+            e.onmouseover = function() {
+                e.children[0].style.transform = 'translateX(96px)'
+                e.children[1].style.transform = 'translateX(-30px)'
+            }
 
-        sliderLink.onmouseover = function() {
-            sliderSpan.style.transform = 'translateX(-30px)'
-            sliderIcon.style.transform = 'translateX(96px)'
-        }
-    
-        sliderLink.onmouseout = function() {
-            sliderSpan.style.transform = sliderIcon.style.transform = ''
-        } 
-    }
-
-    processScroll()
-    processSliderLink()
-
-
-    choiceHRight.forEach((element) => {
-        element.onclick = function(e) {
-            e.stopPropagation()
-            selectBox.forEach((select) => {
-                select.style.display = 'none'
-            })
-            this.nextElementSibling.style.display = 'block'
-            // selectBox.style.display = selectBox.style.display ? 'none' : 'block' 
-        }
-    })
-
-
-    mainMenuIcon.onclick = function() {
-        mainMenu.classList.toggle('active-main-menu')
-    }
-
-
-    document.body.onclick = function() {
-        selectBox.forEach((e) => {
-            e.style.display = 'none'
+            e.onmouseout = function() {
+                e.children[0].style.transform = e.children[1].style.transform = ''
+            }
         })
     }
 
 
+    function processSlider() {
+        
+    }
+
+    processSlider()
+    processScroll()
+    processSliderLink()
+    processBxPaper()
 }
 
 main()
