609bdb352c0ebedc7a237144773e7a68f62b2f07
atk1432 <hiepnguyenvietatk@gmail.com>
2022-02-20 09:08:04 +0700

    add scroll func

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,47 @@ function main() {
     const selectBox = document.querySelectorAll('.select-box')
     const mainMenuIcon = document.querySelector('.main-menu-wrap__icon i')
     const mainMenu = document.querySelector('.main-menu-wrap .col')
+    
+    function processScroll() {
+        const mainMenu = document.querySelector('.main-menu')
+        const slider = document.querySelector('.slider')
+        const siteBranding = document.querySelector('.site-branding')
+        const header = document.querySelector('.header')
+
+        const height = siteBranding.offsetHeight + header.offsetHeight
+        console.log(height)
+
+        document.onscroll = function() {
+            if (document.documentElement.scrollTop >= height) {
+                mainMenu.style.position = 'fixed'
+                slider.style.marginTop = '60px'
+            } else {
+                mainMenu.style.position = ''
+                slider.style.marginTop = ''
+            }
+        }
+    }
+
+
+    function processSliderLink() {
+        const sliderLink = document.querySelector('.slider__item-info-now-text')
+        const sliderSpan = document.querySelector('.slider__item-info-now-text span')      
+        const sliderIcon = document.querySelector('.slider__item-info-now-icon')
+
+
+        sliderLink.onmouseover = function() {
+            sliderSpan.style.transform = 'translateX(-30px)'
+            sliderIcon.style.transform = 'translateX(96px)'
+        }
+    
+        sliderLink.onmouseout = function() {
+            sliderSpan.style.transform = sliderIcon.style.transform = ''
+        } 
+    }
+
+    processScroll()
+    processSliderLink()
+
 
     choiceHRight.forEach((element) => {
         element.onclick = function(e) {
@@ -15,6 +56,7 @@ function main() {
         }
     })
 
+
     mainMenuIcon.onclick = function() {
         mainMenu.classList.toggle('active-main-menu')
     }
@@ -25,6 +67,7 @@ function main() {
             e.style.display = 'none'
         })
     }
+
 
 }
 
