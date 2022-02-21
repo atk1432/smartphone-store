f5629da165783501a0916d4df0755eeeb3b72768
atk1432 <hiepnguyenvietatk@gmail.com>
2022-02-21 22:24:22 +0700

    slider can move

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,12 +26,21 @@ function main() {
             e.style.display = 'none'
         })
     }
+
+    function addSelect(elements, pos, additionClass) {
+        elements.forEach(e => {
+            e.classList.remove(additionClass)
+        })
+
+        elements[pos].classList.add(additionClass)
+    }
     
     function processBxPaper() {
         const bxPapers = document.querySelectorAll('.slider-wrap__bx-paper-item')
         const sliderWrap = document.querySelector('.slider-wrap')
         const angleLeft = document.querySelector('.slider-wrap__angle-left')
         const angleRight = document.querySelector('.slider-wrap__angle-right')
+        const sliderItem = document.querySelectorAll('.slider__item')
 
         sliderWrap.onmouseover = function() {
             angleLeft.style.transform = 'translate(32px)'
@@ -42,18 +51,59 @@ function main() {
             angleLeft.style.transform = angleRight.style.transform = ''
         }
 
-        bxPapers.forEach(function(e) {
+        bxPapers.forEach(function(e, i) {
 
             e.onclick = function() {
 
                 bxPapers.forEach((e) => {
                     e.classList.remove('slider-wrap__bx-paper-item--active')
+
                 })
 
+                var leftPos = i * 1100
+
+                sliderItem.forEach(e => {
+                    e.style.left = '-' + leftPos + 'px'
+                })
+                
                 this.classList.toggle('slider-wrap__bx-paper-item--active')
                 // console.log(this)
             }
         })
+
+        angleLeft.onclick = function(event) {
+            event.preventDefault()
+            sliderItem.forEach(e => {
+                if (e.style.left == '0px' || !e.style.left)  {
+                    var pos = 0
+                } else {
+                    var pos = Number.parseInt(e.style.left.slice(1, -2))
+                }
+                var posResult = pos - 1100 < 0 ? (bxPapers.length - 1) * 1100 : pos - 1100
+
+                e.style.left = '-' + posResult + 'px'
+
+                addSelect(bxPapers, (posResult / 1100) , 'slider-wrap__bx-paper-item--active')
+            })
+        }
+
+        angleRight.onclick = function(event) {
+            event.preventDefault()
+            sliderItem.forEach(e => {
+                if (e.style.left == '0px' || !e.style.left)  {
+                    var pos = 0
+                } else {
+                    var pos = Number.parseInt(e.style.left.slice(1, -2))
+                }
+            
+                var posResult = pos + 1100 > (bxPapers.length - 1) * 1100 ? 
+                                0 : pos + 1100
+
+                e.style.left = '-' + posResult + 'px'
+
+                addSelect(bxPapers, (posResult / 1100), 'slider-wrap__bx-paper-item--active')
+            })
+        }
     }
 
 
@@ -64,7 +114,7 @@ function main() {
         const header = document.querySelector('.header')
 
         const height = siteBranding.offsetHeight + header.offsetHeight
-
+        
         document.onscroll = function() {
             if (document.documentElement.scrollTop >= height) {
                 mainMenu.style.position = 'fixed'
